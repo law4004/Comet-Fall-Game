50c240b9fd6160931ec32ae8ef501bd44af95002
law4004 <88279164+law4004@users.noreply.github.com>
2022-03-26 12:07:45 +0100

    Create comet.py

---
+++ comet.py
@@ -0,0 +1,52 @@
+import pygame
+import random
+
+# creer une classe pour gerer cette comete
+class Comet(pygame.sprite.Sprite):
+
+    def __init__(self, comet_event):
+        super().__init__()
+        # definir l'image associe a cette comete
+        self.image = pygame.image.load('assets/comet.png')
+        self.rect = self.image.get_rect()
+        self.velocity = random.randint(1, 2)
+        self.rect.x = random.randint(20, 800)
+        self.rect.y = - random.randint(0, 800)
+        self.comet_event = comet_event
+
+    def remove(self):
+        self.comet_event.all_comets.remove(self)
+        # jouer le son
+        # self.comet_event.game.sound_manager.play('meteorite')
+
+        # verifier si le nombre de comete est de 0
+        if len(self.comet_event.all_comets) == 0:
+            # remettre la barre a 0
+            self.comet_event.reset_percent()
+            # apparaitre les trois premiers monstres
+            self.comet_event.game.spawn_monster()
+            self.comet_event.game.spawn_monster()
+            self.comet_event.game.spawn_monster()
+
+    def fall(self):
+        self.rect.y += self.velocity
+
+        # ne tombe pas sur le sol
+        if self.rect.y >= 500:
+            # retirer la boule de feu
+            self.remove()
+
+            # si il n'y a plus de boules de feu
+            if len(self.comet_event.all_comets) == 0:
+                # remettre la jauge au depart
+                self.comet_event.reset_percent()
+                self.comet_event.fall_mode = False
+
+        # verifier si la boule de feu touche le joueur
+        if self.comet_event.game.check_collision(
+            self, self.comet_event.game.all_players
+        ):
+            # retirer la boule de feu
+            self.remove()
+            # subir 20 points de degats
+            self.comet_event.game.player.damage(20)
